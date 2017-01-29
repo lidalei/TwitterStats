00d5179087cd34a7b6683a453c1609765b9d13f3
Dalei Li <lidalei151781@gmail.com>
2017-01-29 14:13:25 +0100

    Completed creating table and loading data.

--- src/main/java/hbaseApp/TwitterStats.java
+++ src/main/java/hbaseApp/TwitterStats.java
@@ -1,8 +1,9 @@
 package hbaseApp;
 
+import master2016.StreamTopK;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.*;
@@ -26,6 +27,43 @@ public class TwitterStats {
 
     private static final HTableDescriptor TABLE_DESCRIPTOR = new HTableDescriptor(TableName.valueOf(TABLE_NAME_BYTES));
 
+    private static final byte[] TOP_HASH_TAG1_BYTES = Bytes.toBytes("topHashTag1");
+    private static final byte[] TOP_HASH_TAG2_BYTES = Bytes.toBytes("topHashTag2");
+    private static final byte[] TOP_HASH_TAG3_BYTES = Bytes.toBytes("topHashTag3");
+
+    private static final byte[] TOP_HASH_TAG1_FREQ_BYTES = Bytes.toBytes("topHashTag1Freq");
+    private static final byte[] TOP_HASH_TAG2_FREQ_BYTES = Bytes.toBytes("topHashTag2Freq");
+    private static final byte[] TOP_HASH_TAG3_FREQ_BYTES = Bytes.toBytes("topHashTag3Freq");
+
+
+    private static byte[] topHashTagBytes(int i) {
+        switch(i) {
+            case 1:
+                return TOP_HASH_TAG1_BYTES;
+            case 2:
+                return TOP_HASH_TAG2_BYTES;
+            case 3:
+                return TOP_HASH_TAG3_BYTES;
+            default:
+                return Bytes.toBytes("topHashTag" + i);
+        }
+    }
+
+    private static byte[] topHashTagFreqBytes(int i) {
+        switch(i) {
+            case 1:
+                return TOP_HASH_TAG1_FREQ_BYTES;
+            case 2:
+                return TOP_HASH_TAG2_FREQ_BYTES;
+            case 3:
+                return TOP_HASH_TAG3_FREQ_BYTES;
+            default:
+                return Bytes.toBytes("topHashTag" + i + "Freq");
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
         System.out.println("Hello, HBase!");
@@ -42,20 +80,51 @@ public class TwitterStats {
             String zkHost = args[1];
             System.out.println("zkHost:" + zkHost);
 
-            String startTime = args[2];
-            System.out.println("startTime:" + startTime);
+            byte[] startTsBytes = Bytes.toBytes(args[2]);
+            System.out.println("startTimestamp:" + new String(startTsBytes, StandardCharsets.UTF_8));
 
-            String endTime = args[3];
-            System.out.println("endTime:" + endTime);
+            byte[] endTsBytes = Bytes.toBytes(args[3]);
+            System.out.println("endTimestamp:" + new String(endTsBytes, StandardCharsets.UTF_8));
 
             final int n = Integer.valueOf(args[4]);
             System.out.println("n:" + n);
 
-            String lang = args[5];
-            System.out.println("lang:" + lang);
+            byte[] langBytes = Bytes.toBytes(args[5]);
+            System.out.println("lang:" + new String(langBytes, StandardCharsets.UTF_8));
 
             String outputFolder = args[6];
             System.out.println("outputFolder:" + outputFolder);
+
+            StreamTopK streamTopK = new StreamTopK(n, 10000);
+
+            try{
+                HConnection conn = HConnectionManager.createConnection(CONFIGURATION);
+                HTable table = new HTable(TableName.valueOf(TABLE_NAME_BYTES), conn);
+
+                Scan scan = new Scan(startTsBytes, endTsBytes);
+
+                scan.addFamily(langBytes);
+
+                ResultScanner resScanner = table.getScanner(scan);
+
+                try{
+                    // TODO, deal with hashtags
+                    for(Result res : resScanner) {
+                        // System.out.println("row: " + res);
+                        res.getMap();
+                    }
+                }
+                finally {
+                    resScanner.close();
+                }
+
+                conn.close();
+
+            }
+            catch (IOException e) {
+                System.out.println("IO error while creating a connection.");
+            }
+
 
         }
         else if(mode.equals("2") && args.length >= 7) {// mode 2, given time interval and a list of languages, find top N words for each language
@@ -104,11 +173,40 @@ public class TwitterStats {
 
             Path dataFolderPath = FileSystems.getDefault().getPath(dataFolder);
 
-            // https://docs.oracle.com/javase/tutorial/essential/io/dirs.html#listdir
+            // Create schema, https://docs.oracle.com/javase/tutorial/essential/io/dirs.html#listdir
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(dataFolderPath, "*.log")) {
                 HBaseAdmin admin = new HBaseAdmin(CONFIGURATION);
 
-                // TODO, parallel reads
+                for (Path entry: stream) {
+                    String fileName = entry.getFileName().toString();
+                    String fileNameWithoutExt = fileName.substring(0, fileName.indexOf("."));
+                    System.out.println(fileNameWithoutExt);
+
+                    // add column family
+                    byte[] columnFamilyBytes = Bytes.toBytes(fileNameWithoutExt);
+                    HColumnDescriptor cf = new HColumnDescriptor(columnFamilyBytes);
+                    TABLE_DESCRIPTOR.addFamily(cf);
+                }
+
+                if(admin.tableExists(TABLE_NAME_BYTES)) {
+                    admin.disableTable(TABLE_NAME_BYTES);
+                    admin.deleteTable(TABLE_NAME_BYTES);
+                }
+                admin.createTable(TABLE_DESCRIPTOR);
+            }
+            catch (IOException e) {
+                // IOException can never be thrown by the iteration.
+                // In this snippet, it can only be thrown by newDirectoryStream.
+                System.err.println(e);
+            }
+
+            // add data, store top 3 hashtags into different columns of a column family (language).
+            // In fact, storing them as different versions might be a good idea. Let's stick to this now.
+            // TODO, parallel reads.
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dataFolderPath, "*.log")) {
+                HConnection conn = HConnectionManager.createConnection(CONFIGURATION);
+                HTable table = new HTable(TableName.valueOf(TABLE_NAME_BYTES), conn);
+
                 for (Path entry: stream) {
                     String fileName = entry.getFileName().toString();
                     String fileNameWithoutExt = fileName.substring(0, fileName.indexOf("."));
@@ -117,25 +215,29 @@ public class TwitterStats {
                     // add column family
                     byte[] columnFamilyBytes = Bytes.toBytes(fileNameWithoutExt);
 
-                    HColumnDescriptor cf = new HColumnDescriptor(columnFamilyBytes);
-
-                    TABLE_DESCRIPTOR.addFamily(cf);
-
                     BufferedReader trendingTopicsReader = Files.newBufferedReader(entry, StandardCharsets.UTF_8);
 
                     String trendingTopics = null;
                     while((trendingTopics = trendingTopicsReader.readLine()) != null) {
                         // System.out.println(trendingTopics);
-                        // TODO, split into timestamp, top 3 frequent hashtags
+                        // split into timestamp, top 3 frequent hashtags
+                        String[] topicsArr = trendingTopics.split(",");
+                        byte[] tsKey = Bytes.toBytes(topicsArr[0]);
 
+                        Put put = new Put(tsKey);
+
+                        for(int i = 0; (3 + (i << 1)) < topicsArr.length; ++i) {
+                            byte[] hashTag = Bytes.toBytes(topicsArr[2 + (i << 1)]);
+                            byte[] freq = Bytes.toBytes(topicsArr[3 + (i << 1)]);
+
+                            // add column and value
+                            put.add(columnFamilyBytes, topHashTagBytes(i + 1), hashTag);
+                            put.add(columnFamilyBytes, topHashTagFreqBytes(i + 1), freq);
+                        }
+
+                        table.put(put);
                     }
                 }
-
-                if(admin.tableExists(TABLE_NAME_BYTES)) {
-                    admin.disableTable(TABLE_NAME_BYTES);
-                    admin.deleteTable(TABLE_NAME_BYTES);
-                }
-                admin.createTable(TABLE_DESCRIPTOR);
             }
             catch (IOException e) {
                 // IOException can never be thrown by the iteration.
